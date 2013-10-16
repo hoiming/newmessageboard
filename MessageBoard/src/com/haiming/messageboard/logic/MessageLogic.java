a670f23b78cf0f2f04a3b2fac187060a52d658e2
Haiming Liang <haiming_liang@yahoo.com>
2013-10-16 21:15:07 +0800

    实现了saveMessage方法

--- MessageBoard/src/com/haiming/messageboard/logic/MessageLogic.java
+++ MessageBoard/src/com/haiming/messageboard/logic/MessageLogic.java
@@ -1,6 +1,10 @@
 package com.haiming.messageboard.logic;
 
+import java.util.Date;
+
 import com.haiming.messageboard.bean.Message;
+import com.haiming.messageboard.bean.Theme;
+import com.haiming.messageboard.bean.User;
 import com.haiming.messageboard.dao.Dao;
 import com.haiming.messageboard.dao.SqliteDaoImpl;
 
@@ -10,8 +14,28 @@ public class MessageLogic {
 		dao = new SqliteDaoImpl<Message>();
 	}
 	
-	public void saveMessage(){ 
-		
+	/**
+	 * 保存帖子的内容
+	 * @param t
+	 * @param message
+	 * @param u
+	 * @return
+	 */
+	public Message saveMessage(Theme  t,String message,User u){ 
+		if(t == null || u == null || message == null)
+			return null;
+		Message m = new Message();
+		m.setId(u.getId());
+		m.setThemeid(t.getThemeid());
+		m.setContent(message);
+		m.setCreatedtime(new Date());
+		try {
+			dao.save(m);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return m;
 	}
 
 }
