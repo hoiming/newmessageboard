b57b49f2c29334047cc496956e520ced2fd9ae75
Haiming Liang <haiming_liang@yahoo.com>
2013-10-16 21:15:55 +0800

    添加了方法changeHTML，把敏感字符转义

--- MessageBoard/src/com/haiming/messageboard/utils/MyTools.java
+++ MessageBoard/src/com/haiming/messageboard/utils/MyTools.java
@@ -10,15 +10,27 @@ import java.io.UnsupportedEncodingException;
  */
 public class MyTools {
 
-	public static String toUTF8(String str){ 
+	public static String toUTF8(String str) {
 		String srcEncoding = "ISO-8859-1";
 		String destEncoding = "UTF-8";
 		try {
-			str = new String(str.getBytes(srcEncoding),destEncoding);
+			str = new String(str.getBytes(srcEncoding), destEncoding);
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return str;
+	}
+
+	/**
+	 * 把一些字符转义，例如尖括号
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String changeHTML(String str) {
+		return str.replace("&", str).replace(" ", "&nbsp;").replace("<", "&lt")
+				.replace(">", "&gt;").replace("<", "&lt;")
+				.replace("\r\n", "<br>");
 	}
 }
