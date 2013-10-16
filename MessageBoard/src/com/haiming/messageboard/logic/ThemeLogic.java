e35ec31172b004020cc8fb16c29c74851398d497
Haiming Liang <haiming_liang@yahoo.com>
2013-10-16 16:41:57 +0800

    增加了保存主题的方法

--- MessageBoard/src/com/haiming/messageboard/logic/ThemeLogic.java
+++ MessageBoard/src/com/haiming/messageboard/logic/ThemeLogic.java
@@ -1,7 +1,10 @@
 package com.haiming.messageboard.logic;
 
+import java.util.Date;
+
 import com.haiming.messageboard.bean.Page;
 import com.haiming.messageboard.bean.Theme;
+import com.haiming.messageboard.bean.User;
 import com.haiming.messageboard.dao.Dao;
 import com.haiming.messageboard.dao.SqliteDaoImpl;
 
@@ -24,6 +27,25 @@ public class ThemeLogic {
 		}
 		return null;
 		
+	}
+	
+	/**
+	 * 保存主题
+	 * @param theme
+	 * @param user
+	 */
+	public void saveTheme(String theme,User user){ 
+		Theme t = new Theme();
+		t.setContent(theme);
+		t.setId(user.getId());
+		t.setCreatedtime(new Date());
+		System.out.println(t);
+		try {
+			dao.save(t);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
