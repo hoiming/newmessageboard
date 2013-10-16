cf6371531cf27d5deb0deb0d2428edc888c3e5d0
Haiming Liang <haiming_liang@yahoo.com>
2013-10-16 16:47:26 +0800

    一个工具类，现在的功能是转换字符串编码

---
+++ MessageBoard/src/com/haiming/messageboard/utils/MyTools.java
@@ -0,0 +1,24 @@
+package com.haiming.messageboard.utils;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * 一个工具类，提供字符串编码转换
+ * 
+ * @author Haiming-Liang
+ * 
+ */
+public class MyTools {
+
+	public static String toUTF8(String str){ 
+		String srcEncoding = "ISO-8859-1";
+		String destEncoding = "UTF-8";
+		try {
+			str = new String(str.getBytes(srcEncoding),destEncoding);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return str;
+	}
+}
